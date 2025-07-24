79a28f15b439a416191d34f09331bb8f0db02bae
Thistle <zakaria.hajlaoui@gmail.com>
2025-07-24 18:02:43 +0100

    added healthcheck on api

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 app = FastAPI()
 
+@app.get("/health")
+def health():
+    # Todo
+    # Check api health
+    return None
+
 @app.get("/")
 def root():
     return {"status": "linkedin-scraper running"}
